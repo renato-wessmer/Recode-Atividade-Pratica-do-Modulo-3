21191d8b8bcb833a8a89543e25e30c3204b55e20
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-21 14:22:30 -0300

    Create HospedagemDtoMetadata.java

---
+++ src/dto/HospedagemDtoMetadata.java
@@ -0,0 +1,17 @@
+package dto;
+
+public class HospedagemDtoMetadata {
+
+	public static final String tableName = "hospedagem";
+    public static final String id = "id";
+    public static final String nomeHotel = "nome_hotel";
+    public static final String endereco = "endereco";
+    public static final String estado = "estado";
+    public static final String valorIntegral = "valor_integral";
+    public static final String checkIn = "check_in";
+    public static final String checkOut = "check_out";
+    public static final String numeroHotel = "numero_hotel";
+    public static final String cliente = "fk_cliente_id";
+
+}
+
