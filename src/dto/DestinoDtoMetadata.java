3e39c5d2eb885e74428efd3dc530b0c112012b5e
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-21 14:22:32 -0300

    Create DestinoDtoMetadata.java

---
+++ src/dto/DestinoDtoMetadata.java
@@ -0,0 +1,15 @@
+package dto;
+
+public class DestinoDtoMetadata {
+
+	public static final String tableName = "destino";
+    public static final String id = "id";
+    public static final String nome = "nome";
+    public static final String dataIda = "data_ida";
+    public static final String dataVolta = "data_volta";
+    public static final String valorIntegral = "valor_integral";
+    public static final String cliente = "fk_cliente_id";
+    public static final String cidade = "cidade";
+    public static final String estado = "estado";
+
+}
