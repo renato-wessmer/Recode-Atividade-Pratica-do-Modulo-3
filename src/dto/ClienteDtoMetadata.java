5fbf6cf6fc0ce23164d1ec572de5e6d25d66066c
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-21 14:22:28 -0300

    Create ClienteDtoMetadata.java

---
+++ src/dto/ClienteDtoMetadata.java
@@ -0,0 +1,18 @@
+package dto;
+
+public class ClienteDtoMetadata {
+
+	public static final String tableName = "cliente";
+    public static final String id = "id";
+    public static final String cpf = "cpf";
+    public static final String nomeCompleto = "nome_completo";
+    public static final String rua = "rua";
+    public static final String bairro = "bairro";
+    public static final String cep = "cep";
+    public static final String cidade = "cidade";
+    public static final String estado = "estado";
+    public static final String email = "email";
+    public static final String numero = "numero";
+
+
+}
